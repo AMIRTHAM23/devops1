87fcf783029fd15d66c6eef8ed97fd48551863d3
PRAVEENMAHA1623 <praveensaran4001@gmail.com>
2025-09-05 16:58:12 +0530

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = input.nextLine();
+        System.out.print("Please enter your surname: ");
+        String surname = input.nextLine();
+    }
+}
